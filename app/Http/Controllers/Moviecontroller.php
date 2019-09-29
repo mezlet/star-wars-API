ff25b2c7dc2805246dbdcb59a1b2a1af1734bcc8
mezlet <esiaguchimezieleticia@rocketmail.com>
2019-09-29 14:28:14 +0100

    ft-get-movies: Create endpoint to get movie list

--- app/Http/Controllers/Moviecontroller.php
+++ app/Http/Controllers/Moviecontroller.php
@@ -8,6 +8,7 @@ use App\Services\StarWars;
 use Illuminate\Http\Request;
 use App\Utils\Helpers;
 use Illuminate\Support\Facades\DB;
+use GuzzleHttp\Client;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class MovieController extends Basecontroller{
@@ -15,6 +16,16 @@ class MovieController extends Basecontroller{
     public function getMovie($title){
         try{
             $movies = Helpers::getMovie(ucwords(urldecode($title)));
+            return Helpers::successResponse(200,$movies,'');
+        }catch(\Exception $e){
+            return Helpers::errorResponse(500,'Something went wrong.');
+        }
+    }
+
+    public static function getMovieList()
+    {
+        try{
+            $movies = Helpers::getMovieList();
             return Helpers::successResponse(200,$movies,'');
         }catch(\Exception $e){
             return Helpers::errorResponse(500,'Something went wrong.');
